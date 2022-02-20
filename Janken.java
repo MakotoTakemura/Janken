4c380702d38fe1a6ef8f5076f324d85db7453fd3
EC2 Default User <ec2-user@ip-172-31-23-54.ap-northeast-1.compute.internal>
2022-02-20 05:04:27 +0000

    2nd

--- Janken.java
+++ Janken.java
@@ -1,3 +1,7 @@
+// String chinchinsに関しては好きな名称に変更可能。ただし、数字が先頭にきたり日本語にするのはNG
+// OK: String Oppais NG: String 3janken NG: String 出し手
+// 自然数を表す int名も任意に命名可能 例:computerX, computerY
+// 全角スペースが1個でも混じるとバグる可能性あり
 //以下解答
 int coma = rand.nextInt(3);
 int comb = rand.nextInt(3);
@@ -12,5 +16,6 @@ if(coma == comb){
 } else if(coma == 0 && comb == 1 || coma == 1 && comb == 2 || coma == 2 && comb == 0){
     System.out.println("コンピューターAの勝ち")；
 } else{
-    System.out.println("コンピューターの勝ち")；
+    System.out.println("コンピューターAの負け")；
 }
+
